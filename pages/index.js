5df376401bbe1cb7aeb8e12408d8cdfb784640d0
isitusnoamow <240135@hkis.edu.hk>
2023-03-05 18:23:45 +0800

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,57 @@
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs"
+import { BsSearch } from "react-icons/bs"
+import Link from "next/link"
+import { useRef } from "react"
+import { MdUpload } from "react-icons/md"
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
+import { useRouter } from "next/router"
 
-export default function Home({initialSession, initialUser}){
+export default function Home({initialSession}){
   const session = initialSession
-  const user = initialUser
-  console.log(user)
+  const searchRef = useRef()
+  const supabase = useSupabaseClient()
+  const router = useRouter()
+
+  function signOut() {
+    supabase.auth.signOut()
+    router.push("/")
+  }
+
+  function onSearch(){
+    if(searchRef && searchRef.current){
+        router.push({
+            'pathname': '/results',
+            'query': { q: searchRef.current.value}
+        })
+    }
+  }
+
   return(
     <>
-      <div className="">
-
+    
+      <div className="flex justify-between items-center border-white p-4 border-b-2">
+        <Link href="/" className="text-4xl font-extrabold">OpenMed</Link>
+        <form className="flex-grow px-8 flex items-center justify-between" onSubmit={() => onSearch()}>
+          <input type="search" className="border-black border-2 p-2 rounded-full bg-white flex-grow mx-2 text-black" placeholder="Search OpenMed" ref={searchRef}/>
+          <button type="submit" className="border-black border-2 p-3 rounded-full bg-blue-700"><BsSearch /></button>
+        </form>
+        <div className="flex items-center">
+          {!session ?
+            <>
+              <Link href="/login" className="bg-blue-700 rounded-full p-2 text-white font-semibold">Sign in</Link>
+            </>
+            :
+            <>
+              <h1 className="text-2xl mx-2">{initialSession["user"]["user_metadata"]["name"]}</h1>
+              <Link href="/upload" className="mx-2 text-2xl bg-blue-700 p-2 rounded-full">
+                <MdUpload />
+              </Link>
+              <button onClick={() => signOut()} className="mx-2 bg-blue-700 p-2 rounded-full">Sign Out</button>
+            </>
+          }
+        </div>
       </div>
+      
     </>
   )
 }
@@ -22,7 +65,6 @@ export const getServerSideProps = async (ctx) => {
   return{
       props: {
           initialSession: session,
-          initialUser: session["user"]["user_metadata"]
       }
   }
 
